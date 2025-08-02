7ff69e88dea897cb4210b5815973dce037e2ef9b
chekuri-manikanta <chekurimanikanta12@gmail.com>
2025-08-02 12:31:12 +0530

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,17 +1,15 @@
-// logger.js
 function logger(req, res, next) {
     const logEntry = `${new Date().toISOString()} - ${req.method} ${req.originalUrl}`;
     // Here we just print — in real case, you could write to a file
     process.stdout.write(logEntry + "\n");
     next();
 }
-
 module.exports = logger;
 // logger.js
-function logger(req, res, next) {
+function logger(req, res, next)
+    {
     const logEntry = `${new Date().toISOString()} - ${req.method} ${req.originalUrl}`;
     process.stdout.write(logEntry + "\n");
     next();
 }
-
 module.exports = logger;
